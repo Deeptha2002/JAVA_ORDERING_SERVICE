d52f468252bc5486db4b2311c61c70c9935df4da
Deeptha <deeptha.tirumala@gmail.com>
2026-05-10 19:48:57 +0530

    pom fixes

--- java-backend/src/main/java/com/grocerystore/dto/CategoryDTO.java
+++ java-backend/src/main/java/com/grocerystore/dto/CategoryDTO.java
@@ -1,22 +1,3 @@
-package com.grocerystore.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class CategoryRequestDTO {
-    private String name;
-}
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class CategoryResponseDTO {
-    private Long id;
-    private String name;
-    private List<ProductResponseDTO> products;
-}
